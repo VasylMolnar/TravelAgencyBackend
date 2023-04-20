57b0c3ac0a2c7e3499205b4ed68bb140471b1349
VasylMolnar <vasylmolnar281@gmail.com>
2023-04-20 21:29:57 +0300

    new

--- routers/hotel.js
+++ routers/hotel.js
@@ -1,17 +1,27 @@
 const router = require('express').Router()
 const hotelController = require('../controllers/hotelController')
+const verifyRoles = require('../middleware/verifyRoles')
+const roles = require('../config/roles_list')
 const multer = require('multer')
 const upload = multer()
 
 router
     .route('/')
     .get(hotelController.handleAllHotels)
-    .post(upload.array('image'), hotelController.handleCreateHotel)
+    .post(
+        verifyRoles(roles.Admin),
+        upload.array('image'),
+        hotelController.handleCreateHotel
+    )
 
 router
     .route('/:id')
     .get(hotelController.handleHotel)
-    .put(upload.array('image'), hotelController.handleUpdateHotel)
-    .delete(hotelController.handleDeleteHotel)
+    .put(
+        verifyRoles(roles.Admin),
+        upload.array('image'),
+        hotelController.handleUpdateHotel
+    )
+    .delete(verifyRoles(roles.Admin), hotelController.handleDeleteHotel)
 
 module.exports = router
